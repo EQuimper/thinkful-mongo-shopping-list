d4792d02b4074d0a2442e6141447597fbc861790
EQuimper <quimperemanuel@gmail.com>
2016-05-30 13:20:10 -0600

    Refactor some code

--- routes/item.js
+++ routes/item.js
@@ -1,61 +1,67 @@
 var express = require('express');
 var router = express.Router();
+/*>>>>>>=============================================<<<<<<*/
 
 var Item = require('../models/item');
 
 router
-	// Get one item
+// Get one item
 	.get('/items/:id', (req, res) => {
-		Item.findOne({_id: req.params.id}, (err, item) => {
-			if(err)
+		Item.findOne({ _id: req.params.id }, (err, item) => {
+			if (err)
 				res.status(404).json({
 					message: "Item Not Found!"
 				});
-			res.status(200).json({item});
+			res.status(200).json({ item });
 		})
 	})
 	// Get all item
 	.get('/items', (req, res) => {
 		Item.find((err, items) => {
-			if(err)
+			if (err)
 				res.status(404).json({
 					message: "Items Not Found!"
 				});
 			res.send(items)
 		});
 	})
+	// Create item
 	.post('/items', (req, res) => {
 		var item = new Item(req.body);
 
 		item.save((err) => {
-			if(err)
+			if (err)
 				res.status(400).json({
 					message: "Item Not Created!"
-				})
+				});
 			res.status(200).json({
 				message: "Item Created!"
 			});
 		});
 	})
+	// Update item
 	.put('/items/:id', (req, res) => {
-		Item.findOne({_id: req.params.id}, (err, item) => {
-			if(err)
+		Item.findById({ _id: req.params.id }, (err, item) => {
+			if (err)
 				res.send(err);
 			for (prop in req.body) {
-     			 item[prop] = req.body[prop];
-    		}
+				item[ prop ] = req.body[ prop ];
+			}
 			item.save((err) => {
-				if(err)
-					res.send(err)
+				if (err)
+					res.send(err);
 				res.send('Item Updated!');
 			})
 		})
 	})
+	// Delete item
 	.delete('/items/:id', (req, res) => {
-		Item.remove({_id: req.params.id} , (err, item) => {
-			if(err)
-				res.send(err);
-			res.send('Item Removed!');
+		Item.findById({ _id: req.params.id }, (err, item) => {
+			if (err)
+				res.status(404).send(err);
+			item.remove(() => {
+				res.send('Item Removed!');
+			});
 		});
 	});
 
